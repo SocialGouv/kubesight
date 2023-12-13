50c0708904c2bc63270569aa5110a4274999fec8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-12-13 23:16:59 +0100

    fix(events): sort desc

--- src/components/kube/events.tsx
+++ src/components/kube/events.tsx
@@ -14,6 +14,7 @@ export default function EventsWidget({ namespace }: { namespace: Namespace }) {
         .filter((event) =>
           dayjs(event.lastTimestamp).isAfter(dayjs().subtract(1, "hour"))
         )
+        .sort((a, b) => b.lastTimestamp.getTime() - a.lastTimestamp.getTime())
         .map((event) => (
           <Tooltip
             key={event.metadata.name}
